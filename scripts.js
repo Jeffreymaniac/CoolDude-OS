2ae78d42570ae5caca5c2eb35aa7ce03d16bfccc
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2021-12-27 17:06:22 -0800

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-
-document.body.innerHTML = '<h1>CoolDude OS</h1><p>Please select some settings to customize this operating system.</p>
-';
-
-function setBgImg(url) {
-document.body.style.backgroundImage="url(" + "'" + url + "'" + ")"
-  
-}
